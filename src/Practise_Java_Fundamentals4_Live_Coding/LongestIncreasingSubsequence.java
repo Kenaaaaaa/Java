cda1fab944fbaf63e0561f98dfcdc5d2853ead03
kenaaaaaaa <kenatirana@yahoo.com>
2025-05-16 23:59:51 +0200

    LongestIncreasingSubsequence

---
+++ src/Practise_Java_Fundamentals4_Live_Coding/LongestIncreasingSubsequence.java
@@ -0,0 +1,42 @@
+package Practise_Java_Fundamentals4_Live_Coding;
+
+import java.util.Scanner;
+
+public class LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] nums = new int[10];
+        System.out.println("Shkruaj 10 numra (int):");
+        for (int i = 0; i < 10; i++) {
+            nums[i] = input.nextInt();
+        }
+
+        // Array për të ruajtur gjatësinë e nën-sekuencës më të gjatë që përfundon në secilën pozicion
+        int[] lis = new int[10];
+
+        // Fillimisht, çdo element është një subsekuencë me gjatësi 1
+        for (int i = 0; i < 10; i++) {
+            lis[i] = 1;
+        }
+
+        // Llogarit LIS me algoritëm klasik dinamik
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && lis[i] < lis[j] + 1) {
+                    lis[i] = lis[j] + 1;
+                }
+            }
+        }
+
+        // Gjej maksimumin nga lis[]
+        int max = lis[0];
+        for (int i = 1; i < 10; i++) {
+            if (lis[i] > max) {
+                max = lis[i];
+            }
+        }
+
+        System.out.println("Gjatësia e nën-sekuencës më të gjatë në rritje është: " + max);
+    }
+}
